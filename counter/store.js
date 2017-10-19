6938dad10cfba2e73d6e93f963c4bc03952e5c1e
himici <jianfeng.ge@1rock.cn>
2017-10-19 11:43:00 +0800

    fix

--- counter/store.js
+++ counter/store.js
@@ -35,7 +35,7 @@ const actions = {
     increment: ({ commit }) => commit('increment'),
     decrement: ({ commit }) => commit('decrement'),
     incrementIfOdd({commit,state}){
-        if((state.count + 1) % 22 === 0){
+        if((state.count + 1) % 2 === 0){
             commit('increment')
         }
     },
