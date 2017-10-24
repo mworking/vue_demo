14b00d437080a028aaa3b7314ec0c9c0742a832e
himici <jianfeng.ge@1rock.cn>
2017-10-24 15:12:16 +0800

    add test

--- shopping-cart/components/Cart.vue
+++ shopping-cart/components/Cart.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="cart">
         <h2>Your Cart</h2>
-        <p v-show="!products.length"><i>Please add some products to cart.</i></p>
+        <p v-show="!products.length">
+            <i>Please add some products to cart.</i>
+        </p>
+        测试 v-show 的数据： {{products.length}} <br>
         <ul>
             <li v-for="p in products">
                 {{ p.title }} - {{ p.price | currency }} x {{ p.quantity }}
