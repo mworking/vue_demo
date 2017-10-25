14928f5c97424e2e23867730267c785bc4d6cf29
himici <jianfeng.ge@1rock.cn>
2017-10-25 15:55:38 +0800

    beizhu

--- shopping-cart/store/actions.js
+++ shopping-cart/store/actions.js
@@ -1,5 +1,5 @@
 import * as types from './mutation-types'
-
+// 添加到购物车，产品数量必须大于 0
 export const addToCart = ({ commit }, product) => {
     if (product.inventory > 0) {
         commit(types.ADD_TO_CART, {
