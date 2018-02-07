2b43249aaf45557d619434dae86ee20b411d87c3
himici <jianfeng.ge@1rock.cn>
2018-02-07 16:23:51 +0800

    加える説明

--- component_contact/FatherComponent.vue
+++ component_contact/FatherComponent.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <p>父组件向子组件传值 == 父组件向子组件props中的值赋值，直接把子组件的msgFromFather 拿过来用，赋上一个值ok完事</p>
         <son-component msgFromFather = 'this is data from father'></son-component>
     </div>
 </template>
