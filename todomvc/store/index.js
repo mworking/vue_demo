fa28286b22413677a9167ac2a89414511495c970
himici <jianfeng.ge@1rock.cn>
2018-02-08 14:38:21 +0800

    添加注释

--- todomvc/store/index.js
+++ todomvc/store/index.js
@@ -5,8 +5,16 @@ import plugins from './plugins'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-    state,
-    mutations,
-    plugins
-})
+const test_Object = {state,mutations,plugins}
+
+export default new Vuex.Store(test_Object)
+
+// export default new Vuex.Store({
+//     state,
+//     mutations,
+//     plugins
+// })
+
+
+
+
