7c411c68feb155b691293622d0896d512fc920a8
himici <jianfeng.ge@1rock.cn>
2018-02-08 11:53:29 +0800

    添加注释

--- counter/count_解释.vue
+++ counter/count_解释.vue
@@ -24,22 +24,35 @@
 
     // mapGetters 就是把原先vue在computed中的方法，拿到vuex中来写，通过mapGetters 对应过来
     // methods就是把原先vue在methods中的方法，拿到vuex中来写，通过mapActions 对应过来
+    // vue 中的 data 对应 vuex中的 state
     export default {
+        data(){
+          return {
+              count:0
+          }
+        },
         // 把上面mapGetters换成不用vuex的写法，就是下面的， 所以说vuex可用可不用
         computed: {
             evenOrOdd: state => state.count % 2 === 0 ? 'even' : 'odd'
         },
-        methods:mapActions([
-            'increment',
-            'decrement',
-            'incrementIfOdd',
-            'incrementAsync'
-        ])
+        methods:{
+            increment: ({ commit }) => commit('increment'),
+            decrement: ({ commit }) => commit('decrement'),
+            incrementIfOdd({commit,state}){
+                if((state.count + 1) % 2 === 0){
+                    commit('increment')
+                }
+            },
+            incrementAsync ({commit}){
+                return new Promise((resolve,reject)=>{
+                    setTimeout(()=>{
+                        commit('increment')
+                        resolve()
+                    },1000)
+                })
+            }
+        }
     }
-
-
-
-
 </script>
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,14 @@ var webpack = require('webpack')
 module.exports = {
     entry: {
         // 如果，这儿不写counter的话， [name].js 最后生成的是 main.js
-        // counter: './counter/app.js',
+        counter: './counter/app.js',
         // counter_hot: './counter-hot/app.js',
         // todo_mvc: './todomvc/app.js'
         // List:'./todomvc_jiandan/app.js'
         // shopping_cart: './shopping-cart/app.js',
         //     chat:'./chat/app.js'
         //     contact: './component_contact/app.js'
-        contact_2: './component_contact_2/app.js'
+        // contact_2: './component_contact_2/app.js'
     },
 
 
